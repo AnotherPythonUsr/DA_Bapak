619126163dfe193b8e3a2059dca6a18fb7e0d7c4
PythonUsr <ThrowawayGithub@protonmail.com>
2023-02-21 10:35:37 +0800

    Initial commit

---
+++ SY_Project.py
@@ -0,0 +1,24 @@
+import requests
+
+url = 'http://172.18.58.80/nantes/'
+webpage=requests.get(url)
+
+#print(webpage.text)
+
+print("Status code:")
+print("\t *", webpage.status_code)
+
+h = requests.head(url)
+print("Header:")
+print("**********")
+
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+
+headers = {'User-Agent': 'Mobile'}
+
+url2 = 'http://172.18.58.80/headers.php/'
+request_header = requests.get(url2, headers=headers)
+print(request_header.text)
+
